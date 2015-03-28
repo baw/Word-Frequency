c8cb7ab90952cb4002ce8ba6110b0e841bf258c0
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2015-03-28 14:58:26 -0700

    add count words

--- word_frequency_cloud.js
+++ word_frequency_cloud.js
@@ -1,3 +1,34 @@
 (function () {
+  var words = {};
   
+  var traverseDOM = function (elm, cb) {
+    cb(elm);
+    
+    elm = elm.firstChild;
+    while (elm) {
+      traverseDOM(elm, cb);
+      elm = elm.nextSibling;
+    }
+  };
+  
+  var countWords = function (elm) {
+    if (elm.nodeName !== "#text")  return;
+    
+    var text = elm.textContent.replace(/[^A-Za-z]/g, " ");
+    var ws = text.split(" ");
+    
+    for (var i = 0; i < ws.length; i++) {
+      var word = ws[i].toLowerCase();
+      
+      if (word.length < 4) continue;
+      
+      if (words[word] === undefined) {
+        words[word] = 1;
+      } else {
+        words[word]++;
+      }
+    }
+  };
+  
+  traverseDOM(document.body, countWords);
 })();
